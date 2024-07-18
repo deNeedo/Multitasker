afd4cf9314e6e09fe11934902f702e246a531678
deNeedo <jkudrys@protonmail.com>
2024-07-18 15:25:33 +0200

    Added commands to clear dm chat with bot and command to say thing anonymously on the server

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from discord import Intents, Message, Status
+from discord import Intents, Message, Status, DMChannel
 from discord.ext.commands import Bot, Context
 from logger import Logger
 from config import Config
+from random import randint
 
 class Multitasker(Bot):
     def __init__(self, command_prefix, intents):
@@ -9,9 +10,26 @@ class Multitasker(Bot):
         self.add_commands()
 
     def add_commands(self):
-        @self.command(name='purge')
+        # Send anonymous messages
+        @self.command(name = 'say')
+        async def say(ctx: Context):
+            temp = ctx.message.content.split(' ')
+            await ctx.message.delete()
+            if len(temp) > 1:
+                anonymous_message = ' '.join(temp[1:])
+                await ctx.send(f'{anonymous_message}')
+            else:
+                await ctx.send('Say what?')
+
+        @self.command(name = 'purgedm')
+        async def purgedm(ctx: Context):
+            await ctx.author.create_dm()
+            async for message in ctx.author.dm_channel.history(limit = None):
+                if message.author == bot.user: await message.delete()
+
+        @self.command(name = 'purge')
         async def purge(ctx: Context):
-            await self.change_presence(status = Status.do_not_disturb)
+            # await self.change_presence(status = Status.do_not_disturb)
             if ctx.message.author.name == admin:
                 temp = ctx.message.content.split(' ')
                 if len(temp) == 2:
@@ -25,11 +43,17 @@ class Multitasker(Bot):
                     await ctx.send('This command takes exactly one argument')
             else:
                 await ctx.send('You don\'t have permission to use this command')
-            await self.change_presence(status = Status.online)
-            
+            # await self.change_presence(status = Status.online)
+
     async def on_message(self, message: Message):
-        Logger.message(message.channel, message.author.name, message.content)
-        await self.process_commands(message)
+        if isinstance(message.channel, DMChannel):
+            if message.author == bot.user: return
+            else: await message.channel.send('Send message on the server channel')
+        else:
+            Logger.message(message.channel, message.author.display_name, message.content)
+            if message.author == bot.user: return
+            else:
+                await self.process_commands(message)
 
     async def on_ready(self):
         await self.change_presence(status = Status.online)
